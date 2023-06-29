e17c8f578c23cdd930b681a0207e35edb64541b5
Thomas Lam <purej1983@gmail.com>
2023-06-29 14:24:24 +0100

    feat: basic setup of DI using koin

--- src/main/kotlin/com/thomas/Application.kt
+++ src/main/kotlin/com/thomas/Application.kt
@@ -1,13 +1,21 @@
 package com.thomas
 
+import com.thomas.di.mainModule
 import io.ktor.server.application.*
 import com.thomas.plugins.*
+
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    install(Koin) {
+        slf4jLogger()
+        modules(mainModule)
+    }
     configureSerialization()
     configureSockets()
     configureRouting()
