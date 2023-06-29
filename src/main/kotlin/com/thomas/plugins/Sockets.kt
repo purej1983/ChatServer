0c36ddb18fb58106caadd1bfc7f1cec1afd35165
purej1983 <purej1983@gmail.com>
2023-06-29 12:47:25 +0100

    feat: websocket init setup (#1)
    * feat: init setup for the web socket server
    
    * test: unit test for the websocket

---
+++ src/main/kotlin/com/thomas/plugins/Sockets.kt
@@ -0,0 +1,44 @@
+package com.thomas.plugins
+
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import java.util.*
+import kotlin.collections.LinkedHashSet
+import com.thomas.Connection
+
+fun Application.configureSockets() {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/chat") { // websocketSession
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            send("You are connected!")
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
+            }
+        }
+    }
+}
