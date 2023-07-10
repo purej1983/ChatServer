29e437a5d217866ec713cf8218391b5de0c7cabc
Thomas Lam <purej1983@gmail.com>
2023-07-10 18:01:32 +0100

    fix: update to not include secrets in source

--- src/main/kotlin/com/thomas/dao/DatabaseFactory.kt
+++ src/main/kotlin/com/thomas/dao/DatabaseFactory.kt
@@ -9,8 +9,16 @@ import org.jetbrains.exposed.sql.transactions.experimental.*
 object DatabaseFactory {
     fun init(config: ApplicationConfig) {
         val driverClassName = config.property("storage.driverClassName").getString()
-        val jdbcURL = config.property("storage.jdbcURL").getString()
-        val database = Database.connect(jdbcURL, driverClassName)
+        val connectionString = config.property("storage.connectionString").getString()
+        val dbUser = config.property("storage.dbUsername").getString()
+        val dbPass = config.property("storage.dbPassword").getString()
+        val database = Database.connect(
+            url = connectionString,
+            driver = driverClassName,
+            user = dbUser,
+            password = dbPass
+        )
+
         transaction(database) {
             SchemaUtils.create(ChatRooms, Users, ChatRoomsUsers)
         }
